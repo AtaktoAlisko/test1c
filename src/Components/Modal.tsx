61ed39d019a96b460fdc80cdc7d4f42b52eef5c1
Алибек Мурат <alibekmurat@MacBook-Pro-Alibek.local>
2024-08-11 16:06:27 +0500

    third

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -10,8 +10,6 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, toggleModal }) => {
   const form = useRef<HTMLFormElement>(null); // Always call useRef here
 
-  if (!isOpen) return null; // Return early if the modal is not open
-
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -41,8 +39,10 @@ const Modal: React.FC<ModalProps> = ({ isOpen, toggleModal }) => {
     <div
       id="authentication-modal"
       tabIndex={-1}
-      aria-hidden="true"
-      className="fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-60 backdrop-blur-sm"
+      aria-hidden={!isOpen} // Accessibility improvement
+      className={`fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-60 backdrop-blur-sm ${
+        isOpen ? "block" : "hidden"
+      }`}
     >
       <div className="relative p-6 w-full max-w-lg">
         <div className="relative bg-white dark:bg-gray-800 rounded-lg shadow-lg">
@@ -72,71 +72,73 @@ const Modal: React.FC<ModalProps> = ({ isOpen, toggleModal }) => {
               <span className="sr-only">Close modal</span>
             </button>
           </div>
-          <form
-            ref={form}
-            onSubmit={sendEmail}
-            className="p-6 bg-white dark:bg-gray-800 rounded-lg"
-          >
-            <h2 className="text-3xl font-bold mb-6 text-center text-gray-900 dark:text-gray-100">
-              Contact Us
-            </h2>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                htmlFor="name"
-              >
-                Ваша Имя:
-              </label>
-              <input
-                className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
-                id="name"
-                type="text"
-                name="user_name"
-                placeholder="Your Name"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Email:
-              </label>
-              <input
-                className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
-                id="email"
-                type="email"
-                name="user_email"
-                placeholder="Your Email"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-                htmlFor="message"
-              >
-                Сообщение:
-              </label>
-              <textarea
-                className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
-                id="message"
-                name="message"
-                placeholder="Your Message"
-                rows={5}
-                required
-              ></textarea>
-            </div>
-            <div className="flex items-center justify-center">
-              <button
-                className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-colors"
-                type="submit"
-              >
-                Отправить
-              </button>
-            </div>
-          </form>
+          {isOpen && (
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="p-6 bg-white dark:bg-gray-800 rounded-lg"
+            >
+              <h2 className="text-3xl font-bold mb-6 text-center text-gray-900 dark:text-gray-100">
+                Contact Us
+              </h2>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+                  htmlFor="name"
+                >
+                  Ваша Имя:
+                </label>
+                <input
+                  className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
+                  id="name"
+                  type="text"
+                  name="user_name"
+                  placeholder="Your Name"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email:
+                </label>
+                <input
+                  className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
+                  id="email"
+                  type="email"
+                  name="user_email"
+                  placeholder="Your Email"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+                  htmlFor="message"
+                >
+                  Сообщение:
+                </label>
+                <textarea
+                  className="shadow-sm appearance-none border border-gray-300 dark:border-gray-600 rounded-lg w-full py-3 px-4 text-gray-900 dark:text-gray-200 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
+                  id="message"
+                  name="message"
+                  placeholder="Your Message"
+                  rows={5}
+                  required
+                ></textarea>
+              </div>
+              <div className="flex items-center justify-center">
+                <button
+                  className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-colors"
+                  type="submit"
+                >
+                  Отправить
+                </button>
+              </div>
+            </form>
+          )}
         </div>
       </div>
     </div>
